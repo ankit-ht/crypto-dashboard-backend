e9ebee93fa7babe00091486ef64cc2bf95d429dc
ankit-ht <ankitp@heaptrace.com>
2025-07-28 12:12:25 +0530

    Completed

--- backend/models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const productSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  price: Number,
-});
-
-module.exports = mongoose.model("Product", productSchema);
